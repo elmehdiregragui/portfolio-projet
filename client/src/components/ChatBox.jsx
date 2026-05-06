c9208b2e0f4103afbfec585ce9677337085d13ad
Elmehdi Regragui <elmehdiregragui277@gmail.com>
2026-05-06 03:01:10 -0400

    connexion render

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://portfolio-projet-1.onrender.com");
 
 function ChatBox() {
   const [open, setOpen] = useState(false);
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://portfolio-projet-1.onrender.com");
 
 function Chat() {
   const [message, setMessage] = useState("");
